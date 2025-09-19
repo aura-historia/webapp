c1a40cf3682b456d49159366b1b09a583a65b741
Erwin Bause <erwinbause23@gmail.com>
2025-09-19 14:11:17 +0200

    #12: implement first version for the header

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import {Button} from "./ui/button.tsx";
+import {Link} from "@tanstack/react-router";
+
+export function Header() {
+    return (
+        <header className="flex items-center justify-between sticky top-0 px-4">
+
+            <Link to="/">
+                <img className="h-60 w-auto" src="/blitzfilter-logo.png" alt="Blitzfilter Logo"/>
+            </Link>
+
+            <div className="flex items-center gap-4">
+                <Button>Registrieren</Button>
+                <Button>Einloggen</Button>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,13 @@
-import { TanstackDevtools } from "@tanstack/react-devtools";
-import { HeadContent, Scripts, createRootRouteWithContext } from "@tanstack/react-router";
-import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+import {TanstackDevtools} from "@tanstack/react-devtools";
+import {HeadContent, Scripts, createRootRouteWithContext} from "@tanstack/react-router";
+import {TanStackRouterDevtoolsPanel} from "@tanstack/react-router-devtools";
 
 import TanStackQueryDevtools from "../integrations/tanstack-query/devtools";
 
 import appCss from "../styles.css?url";
 
-import type { QueryClient } from "@tanstack/react-query";
+import type {QueryClient} from "@tanstack/react-query";
+import Header from "@/components/Header.tsx";
 
 interface MyRouterContext {
     queryClient: QueryClient;
@@ -37,28 +38,29 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
     shellComponent: RootDocument,
 });
 
-function RootDocument({ children }: { children: React.ReactNode }) {
+function RootDocument({children}: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <head>
-                <HeadContent />
-            </head>
-            <body>
-                {children}
-                <TanstackDevtools
-                    config={{
-                        position: "bottom-left",
-                    }}
-                    plugins={[
-                        {
-                            name: "Tanstack Router",
-                            render: <TanStackRouterDevtoolsPanel />,
-                        },
-                        TanStackQueryDevtools,
-                    ]}
-                />
-                <Scripts />
-            </body>
+        <head>
+            <HeadContent/>
+        </head>
+        <body>
+        <Header/>
+        {children}
+        <TanstackDevtools
+            config={{
+                position: "bottom-left",
+            }}
+            plugins={[
+                {
+                    name: "Tanstack Router",
+                    render: <TanStackRouterDevtoolsPanel/>,
+                },
+                TanStackQueryDevtools,
+            ]}
+        />
+        <Scripts/>
+        </body>
         </html>
     );
 }
