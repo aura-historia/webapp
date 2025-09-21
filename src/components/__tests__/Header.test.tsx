93a1badbd073003d0c34ede5bb19e322756ab497
Erwin Bause <erwinbause23@gmail.com>
2025-09-21 16:58:23 +0200

    test#12: add simple testcases for header component

---
+++ src/components/__tests__/Header.test.tsx
@@ -0,0 +1,26 @@
+import {act, screen} from "@testing-library/react";
+import {describe, beforeEach, expect, test} from "vitest";
+import {Header} from "../Header";
+import {renderWithRouter} from "@/test/utils.tsx";
+
+
+describe("Header Component", () => {
+    beforeEach(async () => {
+        await act(() => {
+            renderWithRouter(<Header/>);
+        });
+    });
+
+    test("should render Blitzfilter logo link", () => {
+        const logoLink = screen.getByText("Blitzfilter");
+        expect(logoLink).toBeInTheDocument();
+        expect(logoLink.closest("a")).toHaveAttribute("href", "/");
+    });
+
+    test("should renders auth buttons with correct text", () => {
+        expect(screen.getByText("Registrieren")).toBeInTheDocument();
+        expect(screen.getByText("Einloggen")).toBeInTheDocument();
+    });
+
+
+});
---
+++ src/test/utils.tsx
@@ -0,0 +1,57 @@
+import {
+    Outlet,
+    RouterProvider,
+    createMemoryHistory,
+    createRootRouteWithContext,
+    createRoute,
+    createRouter,
+} from "@tanstack/react-router";
+import {render} from "@testing-library/react";
+import type {ReactNode} from "react";
+
+const rootRoute = createRootRouteWithContext()({
+    component: () => <Outlet/>, // entry point to render children
+});
+
+const indexRoute = createRoute({
+    getParentRoute: () => rootRoute,
+    path: "/",
+    component: () => <div>{"Index"}</div>,
+});
+
+let injectedChildren: ReactNode = null;
+
+const testRoute = createRoute({
+    getParentRoute: () => rootRoute,
+    path: "/test",
+    component: () => <>{injectedChildren}</>,
+});
+
+const routeTree = rootRoute.addChildren([indexRoute, testRoute]);
+
+interface TestRouterWrapperProps {
+    children: ReactNode;
+    initialEntries?: string[];
+}
+
+export function TestRouterWrapper({
+                                      children,
+                                      initialEntries = ["/test"],
+                                  }: TestRouterWrapperProps) {
+    injectedChildren = children;
+
+    const router = createRouter({
+        routeTree,
+        history: createMemoryHistory({initialEntries}),
+        context: {},
+    });
+
+    return <RouterProvider router={router}/>;
+}
+
+export function renderWithRouter(
+    ui: React.ReactElement,
+    options: Omit<TestRouterWrapperProps, "children"> = {},
+) {
+    return render(<TestRouterWrapper {...options}>{ui}</TestRouterWrapper>);
+}
