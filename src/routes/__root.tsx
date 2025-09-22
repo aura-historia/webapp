f274013cd8bb868d8beecc9e5e1e10c43952e5b4
Erwin Bause <erwinbause23@gmail.com>
2025-09-22 18:24:16 +0200

    fix#12: Move the footer to the /components/common folder

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -7,7 +7,7 @@ import TanStackQueryDevtools from "../integrations/tanstack-query/devtools";
 import appCss from "../styles.css?url";
 
 import Header from "@/components/common/Header.tsx";
-import { Footer } from "@/components/Footer.tsx";
+import { Footer } from "@/components/common/Footer.tsx";
 import type { QueryClient } from "@tanstack/react-query";
 
 interface MyRouterContext {
