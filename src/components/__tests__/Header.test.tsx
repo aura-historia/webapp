19e8b34de35962ee8e1e76bf9f5e810b8b29e78e
Erwin Bause <erwinbause23@gmail.com>
2025-09-22 20:00:54 +0200

    fix#12: use it instead of test

--- src/components/__tests__/Header.test.tsx
+++ src/components/__tests__/Header.test.tsx
@@ -1,22 +1,22 @@
-import { renderWithRouter } from "@/test/utils.tsx";
-import { act, screen } from "@testing-library/react";
-import { beforeEach, describe, expect, test } from "vitest";
-import { Header } from "../common/Header.tsx";
+import {renderWithRouter} from "@/test/utils.tsx";
+import {act, screen} from "@testing-library/react";
+import {beforeEach, describe, expect, it} from "vitest";
+import {Header} from "../common/Header.tsx";
 
 describe("Header Component", () => {
     beforeEach(async () => {
         await act(() => {
-            renderWithRouter(<Header />);
+            renderWithRouter(<Header/>);
         });
     });
 
-    test("should render Blitzfilter logo link", () => {
+    it("should render Blitzfilter logo link", () => {
         const logoLink = screen.getByText("Blitzfilter");
         expect(logoLink).toBeInTheDocument();
         expect(logoLink.closest("a")).toHaveAttribute("href", "/");
     });
 
-    test("should renders auth buttons with correct text", () => {
+    it("should renders auth buttons with correct text", () => {
         expect(screen.getByText("Registrieren")).toBeInTheDocument();
         expect(screen.getByText("Einloggen")).toBeInTheDocument();
     });
