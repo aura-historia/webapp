518c7d6066a17546cbf63b701cec12aa5cbc5944
Luca Franke <luca.franke@appsfactory.de>
2025-10-14 09:57:36 +0200

    #42 add i18next config

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import DiscoverSection from "@/components/landing-page/DiscoverSection.tsx";
 import SearchContainer from "@/components/landing-page/SearchContainer.tsx";
 import { createFileRoute } from "@tanstack/react-router";
+import "../i18n/i18n.ts";
 
 export const Route = createFileRoute("/")({
     component: LandingPage,
