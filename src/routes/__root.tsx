ace2f372f3492f1ae12e1becd8b3c5cc8162ac11
Luca <luca.franke21+github@gmail.com>
2025-10-16 23:00:42 +0200

    add manual dev environment check

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -50,18 +50,20 @@ function RootDocument({ children }: { readonly children: React.ReactNode }) {
                     <main className="flex-1">{children}</main>
                     <Footer />
                 </div>
-                <TanStackDevtools
-                    config={{
-                        position: "bottom-left",
-                    }}
-                    plugins={[
-                        {
-                            name: "Tanstack Router",
-                            render: <TanStackRouterDevtoolsPanel />,
-                        },
-                        TanStackQueryDevtools,
-                    ]}
-                />
+                {import.meta.env.DEV && (
+                    <TanStackDevtools
+                        config={{
+                            position: "bottom-left",
+                        }}
+                        plugins={[
+                            {
+                                name: "Tanstack Router",
+                                render: <TanStackRouterDevtoolsPanel />,
+                            },
+                            TanStackQueryDevtools,
+                        ]}
+                    />
+                )}
                 <Scripts />
             </body>
         </html>
