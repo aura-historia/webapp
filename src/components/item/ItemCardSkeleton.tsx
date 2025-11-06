b9da1b7dd71ebd9a73a125ad10ec4909d18f4da9
Luca Franke <84925848+lfranke42@users.noreply.github.com>
2025-11-06 21:54:30 +0100

    Add pattern background for all pages except landing page (#173)

--- src/components/item/ItemCardSkeleton.tsx
+++ src/components/item/ItemCardSkeleton.tsx
@@ -8,18 +8,18 @@ export function ItemCardSkeleton() {
             className={"flex flex-col lg:flex-row p-8 gap-4 shadow-md min-w-0 w-full"}
         >
             <div className={"flex-shrink-0 flex lg:justify-start justify-center"}>
-                <Skeleton className={"size-48 rounded-lg bg-muted"} />
+                <Skeleton className={"size-48 rounded-lg"} />
             </div>
             <div className={"flex flex-col min-w-0 flex-1 justify-between"}>
                 <div className={"flex flex-row justify-between w-full gap-4"}>
                     <div className={"flex flex-col gap-2 overflow-hidden"}>
-                        <Skeleton className={"h-8 w-64 bg-muted"} />
-                        <Skeleton className={"h-6 w-40 bg-muted"} />
-                        <Skeleton className={"h-5 w-24 bg-muted rounded-full"} />
+                        <Skeleton className={"h-8 w-64"} />
+                        <Skeleton className={"h-6 w-40"} />
+                        <Skeleton className={"h-5 w-24 rounded-full"} />
                     </div>
 
                     <div className={"ml-auto flex-shrink-0"}>
-                        <Skeleton className={"size-10 rounded-md bg-muted"} />
+                        <Skeleton className={"size-10 rounded-md"} />
                     </div>
                 </div>
 
@@ -28,15 +28,11 @@ export function ItemCardSkeleton() {
                         "flex flex-col lg:flex-row gap-4 lg:items-end lg:gap-0 justify-between w-full mt-4 lg:mt-0"
                     }
                 >
-                    <Skeleton className={"h-8 w-32 bg-muted"} />
+                    <Skeleton className={"h-8 w-32"} />
 
                     <div className={"flex flex-col gap-2 lg:items-end flex-shrink-0"}>
-                        <Skeleton
-                            className={"h-10 w-full lg:w-24 flex-shrink-0 bg-muted rounded-md"}
-                        />
-                        <Skeleton
-                            className={"h-10 w-full lg:w-48 flex-shrink-0 bg-muted rounded-md"}
-                        />
+                        <Skeleton className={"h-10 w-full lg:w-24 flex-shrink-0 rounded-md"} />
+                        <Skeleton className={"h-10 w-full lg:w-48 flex-shrink-0 rounded-md"} />
                     </div>
                 </div>
             </div>
