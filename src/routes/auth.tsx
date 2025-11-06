be3f4fd317c8010624ddaa317a83ec88640e6631
Luca <luca.franke21+github@gmail.com>
2025-11-06 21:48:48 +0100

    #143 fix transparent amplify components

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -42,7 +42,7 @@ function AuthPage() {
                     }
                     return (
                         <div className="min-h-screen flex items-center justify-center">
-                            <div className="w-12 h-12 border-4 border-primary/20 border-t-primary rounded-full animate-spin"></div>
+                            <div className="w-12 h-12 border-4 border-t-primary rounded-full animate-spin"></div>
                         </div>
                     );
                 }}
