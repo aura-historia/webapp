bce5fee0cd1ca9b77dc8b318843c417b72b673df
Luca <luca.franke21+github@gmail.com>
2026-01-11 17:06:55 +0100

    #284 extract components

--- src/components/imprint/Imprint.tsx
+++ src/components/imprint/Imprint.tsx
@@ -6,6 +6,7 @@ import { H3 } from "@/components/typography/H3.tsx";
 import { H4 } from "@/components/typography/H4.tsx";
 import { IMPRINT_LOCALE_MAP } from "@/assets/content/imprint/imprint-asset-map.ts";
 import { Card } from "@/components/ui/card.tsx";
+import type { HTMLProps, ReactNode } from "react";
 
 export function Imprint() {
     const { t, i18n } = useTranslation();
@@ -14,37 +15,54 @@ export function Imprint() {
         <div className="max-w-6xl lg:px-4 flex flex-col py-8 ml-8 mr-8 gap-2 lg:ml-auto lg:mr-auto">
             <Card className="gap-4 flex-col flex p-8">
                 <H1 className="pb-4">{t("imprint.title")}</H1>
-                <Markdown
-                    components={{
-                        h1(props) {
-                            const { node, children, ...rest } = props;
-                            return <H1 {...rest}>{children}</H1>;
-                        },
-                        h2(props) {
-                            const { node, children, ...rest } = props;
-                            return <H2 {...rest}>{children}</H2>;
-                        },
-                        h3(props) {
-                            const { node, children, ...rest } = props;
-                            return <H3 {...rest}>{children}</H3>;
-                        },
-                        h4(props) {
-                            const { node, children, ...rest } = props;
-                            return <H4 {...rest}>{children}</H4>;
-                        },
-                        a(props) {
-                            const { node, children, ...rest } = props;
-                            return (
-                                <a className="underline" {...rest}>
-                                    {children}
-                                </a>
-                            );
-                        },
-                    }}
-                >
+                <Markdown components={{ h1, h2, h3, h4, a }}>
                     {IMPRINT_LOCALE_MAP[i18n.language] || IMPRINT_LOCALE_MAP.en}
                 </Markdown>
             </Card>
         </div>
+    );
+}
+
+function h1({
+    node,
+    children,
+    ...props
+}: { node?: unknown; children?: ReactNode } & HTMLProps<HTMLHeadingElement>) {
+    return <H1 {...props}>{children}</H1>;
+}
+
+function h2({
+    node,
+    children,
+    ...props
+}: { node?: unknown; children?: ReactNode } & HTMLProps<HTMLHeadingElement>) {
+    return <H2 {...props}>{children}</H2>;
+}
+
+function h3({
+    node,
+    children,
+    ...props
+}: { node?: unknown; children?: ReactNode } & HTMLProps<HTMLHeadingElement>) {
+    return <H3 {...props}>{children}</H3>;
+}
+
+function h4({
+    node,
+    children,
+    ...props
+}: { node?: unknown; children?: ReactNode } & HTMLProps<HTMLHeadingElement>) {
+    return <H4 {...props}>{children}</H4>;
+}
+
+function a({
+    node,
+    children,
+    ...props
+}: { node?: unknown; children?: ReactNode } & HTMLProps<HTMLAnchorElement>) {
+    return (
+        <a className="underline" {...props}>
+            {children}
+        </a>
     );
 }
