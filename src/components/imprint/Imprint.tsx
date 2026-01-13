d9c31d99f659342be8ac9c892431e140548de157
Luca Franke <84925848+lfranke42@users.noreply.github.com>
2026-01-13 14:07:38 +0100

    #284 Imprint (#303)

---
+++ src/components/imprint/Imprint.tsx
@@ -0,0 +1,68 @@
+import { useTranslation } from "react-i18next";
+import { H1 } from "@/components/typography/H1.tsx";
+import Markdown from "react-markdown";
+import { H2 } from "@/components/typography/H2.tsx";
+import { H3 } from "@/components/typography/H3.tsx";
+import { H4 } from "@/components/typography/H4.tsx";
+import { IMPRINT_LOCALE_MAP } from "@/assets/content/imprint/imprint-asset-map.ts";
+import { Card } from "@/components/ui/card.tsx";
+import type { HTMLProps, ReactNode } from "react";
+
+export function Imprint() {
+    const { t, i18n } = useTranslation();
+
+    return (
+        <div className="max-w-6xl lg:px-4 flex flex-col py-8 ml-8 mr-8 gap-2 lg:ml-auto lg:mr-auto">
+            <Card className="gap-4 flex-col flex p-8">
+                <H1 className="pb-4">{t("imprint.title")}</H1>
+                <Markdown components={{ h1, h2, h3, h4, a }}>
+                    {IMPRINT_LOCALE_MAP[i18n.language] || IMPRINT_LOCALE_MAP.en}
+                </Markdown>
+            </Card>
+        </div>
+    );
+}
+
+function h1({
+    node,
+    children,
+    ...props
+}: { node?: unknown; children?: ReactNode } & HTMLProps<HTMLHeadingElement>) {
+    return <H1 {...props}>{children}</H1>;
+}
+
+function h2({
+    node,
+    children,
+    ...props
+}: { node?: unknown; children?: ReactNode } & HTMLProps<HTMLHeadingElement>) {
+    return <H2 {...props}>{children}</H2>;
+}
+
+function h3({
+    node,
+    children,
+    ...props
+}: { node?: unknown; children?: ReactNode } & HTMLProps<HTMLHeadingElement>) {
+    return <H3 {...props}>{children}</H3>;
+}
+
+function h4({
+    node,
+    children,
+    ...props
+}: { node?: unknown; children?: ReactNode } & HTMLProps<HTMLHeadingElement>) {
+    return <H4 {...props}>{children}</H4>;
+}
+
+function a({
+    node,
+    children,
+    ...props
+}: { node?: unknown; children?: ReactNode } & HTMLProps<HTMLAnchorElement>) {
+    return (
+        <a className="underline" {...props}>
+            {children}
+        </a>
+    );
+}
