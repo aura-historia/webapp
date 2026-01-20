0eed42504a00e8591f28113531769cbd87ed8c16
Erwin Bause <erwinbause23@gmail.com>
2026-01-20 13:50:48 +0100

    fix: disable SSR for auth routes to prevent redirect flash

--- src/routes/_auth.tsx
+++ src/routes/_auth.tsx
@@ -2,6 +2,7 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 import { getCurrentUser } from "@aws-amplify/auth";
 
 export const Route = createFileRoute("/_auth")({
+    ssr: false,
     beforeLoad: async ({ location }) => {
         try {
             const user = await getCurrentUser();
