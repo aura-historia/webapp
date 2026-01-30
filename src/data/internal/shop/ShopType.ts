bfcbe39b3ec1cd7d5520bfa02f1d43a0cb56a8be
Erwin Bause <erwinbause23@gmail.com>
2026-01-30 17:06:31 +0100

    fix#358: Add BackendShopType to exclude UNKNOWN for backend compatibility

--- src/data/internal/shop/ShopType.ts
+++ src/data/internal/shop/ShopType.ts
@@ -2,6 +2,9 @@ export const SHOP_TYPES = ["AUCTION_HOUSE", "COMMERCIAL_DEALER", "MARKETPLACE", 
 
 export type ShopType = (typeof SHOP_TYPES)[number];
 
+// FE accepts UNKNOWN (because of the mapper), but BE dont accepts (and didnt know) UNKNOWN
+export type BackendShopType = Exclude<ShopType, "UNKNOWN">;
+
 export const SHOP_TYPE_TRANSLATION_CONFIG = {
     AUCTION_HOUSE: {
         translationKey: "shopType.auctionHouse",
@@ -30,7 +33,7 @@ export function parseShopType(shopType?: string): ShopType {
     }
 }
 
-export function mapToBackendShopType(shopType?: ShopType): ShopType | undefined {
+export function mapToBackendShopType(shopType?: ShopType): BackendShopType | undefined {
     if (!shopType) return undefined;
 
     switch (shopType) {
