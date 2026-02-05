22dca13750e1bbfaad77cfb9679f3f7159b001d5
Luca <luca.franke21+github@gmail.com>
2026-02-05 19:30:10 +0100

    #341: fix tests

--- src/data/internal/product/ProductImageData.ts
+++ src/data/internal/product/ProductImageData.ts
@@ -10,7 +10,7 @@ type ProhibitedContentType = "UNKNOWN" | "NONE" | "NAZI_GERMANY";
 export function mapToInternalProductImage(apiData: ProductImageData): ProductImage | undefined {
     if (!URL.canParse(apiData.url)) return undefined;
 
-    const url = URL.parse(apiData.url + "asdf");
+    const url = URL.parse(apiData.url);
     if (!url) return undefined;
 
     return {
