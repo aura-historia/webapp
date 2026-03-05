b35bcb80acc5b0e7742aaf90e1652d756b821424
Luca <luca_niklas.franke@stud.htwk-leipzig.de>
2026-03-05 10:54:45 +0100

    #445: add api fetching for category overview items

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,8 +7,19 @@ import TestimonialsSection from "@/components/landing-page/testimonials-section/
 import { createFileRoute } from "@tanstack/react-router";
 import { generatePageHeadMeta } from "@/lib/pageHeadMeta.ts";
 import { env } from "@/env";
+import CategoriesSection from "@/components/landing-page/categories-section/CategoriesSection";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { getCategoriesOptions } from "@/client/@tanstack/react-query.gen";
+import { mapToCategoryOverview } from "@/data/internal/category/CategoryOverview.ts";
+import { parseLanguage } from "@/data/internal/common/Language.ts";
+import i18n from "@/i18n/i18n.ts";
 
 export const Route = createFileRoute("/")({
+    loader: async ({ context: { queryClient } }) => {
+        await queryClient.ensureQueryData(
+            getCategoriesOptions({ query: { language: parseLanguage(i18n.language) } }),
+        );
+    },
     head: () =>
         generatePageHeadMeta({
             pageKey: "home",
@@ -18,9 +29,13 @@ export const Route = createFileRoute("/")({
 });
 
 function LandingPage() {
+    const { data: categoriesData } = useSuspenseQuery(getCategoriesOptions());
+    const categories = categoriesData.map(mapToCategoryOverview);
+
     return (
         <>
             <HeroSection />
+            <CategoriesSection categories={categories} />
             <DiscoverSection />
             <FeaturesSection />
             <HowItWorksSection />
