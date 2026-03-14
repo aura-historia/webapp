1d88eaf2079b8aeda8e34b643c6177545bd9c538
Luca <luca_niklas.franke@stud.htwk-leipzig.de>
2026-03-14 17:18:28 +0100

    #446: add stage google analytics tag

--- src/lib/tracking/gaTrackingHelpers.ts
+++ src/lib/tracking/gaTrackingHelpers.ts
@@ -6,10 +6,10 @@ export function sendPageViewEvent(
     searchParams: Record<string, unknown>,
 ) {
     // These shouldn't be contained - but we filter anyway to make sure we never leak them to GA
-    const FORBIDDEN_PARAMS = ["token", "password", "email", "reset_key", "session_id"];
+    const FORBIDDEN_PARAMS = new Set(["token", "password", "email", "reset_key", "session_id"]);
     const safeParams = Object.keys(searchParams).reduce(
         (acc, key) => {
-            if (!FORBIDDEN_PARAMS.includes(key.toLowerCase())) {
+            if (!FORBIDDEN_PARAMS.has(key.toLowerCase())) {
                 acc[key] = searchParams[key];
             }
             return acc;
--- src/lib/tracking/googleAnalytics.ts
+++ src/lib/tracking/googleAnalytics.ts
@@ -1,7 +1,11 @@
 import ReactGA from "react-ga4";
+import { env } from "@/env.ts";
 
+const TRACKING_TAG_STAGE = "G-25SPFBNNC2";
 const TRACKING_TAG = "G-HL1MJKQBZR";
 
-ReactGA.initialize(TRACKING_TAG, {
+const isRunningInProd = env.VITE_APP_URL === "https://aura-historia.com";
+
+ReactGA.initialize(isRunningInProd ? TRACKING_TAG : TRACKING_TAG_STAGE, {
     gtagOptions: { send_page_view: false },
 });
