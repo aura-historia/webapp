73c9af22bb1d49eb3fffa077e86397ecc19617ed
Luca <luca.franke21+github@gmail.com>
2026-03-31 19:23:34 +0200

    #00: enable backwards comp flag for ReactGA, remove tsconfig paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,14 @@
-import {defineConfig} from 'vite'
-import {tanstackStart} from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
-import {cloudflare} from '@cloudflare/vite-plugin'
-import {devtools} from '@tanstack/devtools-vite'
+import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
+import { devtools } from "@tanstack/devtools-vite";
 
-
-const EXCLUDED_ROUTES = new Set(['/account', '/watchlist', '/search', '/api/', '/login'])
+const EXCLUDED_ROUTES = new Set(["/account", "/watchlist", "/search", "/api/", "/login"]);
 
 export default defineConfig({
     plugins: [
-        // this is the plugin that enables path aliases
-        viteTsConfigPaths({
-            projects: ['./tsconfig.json'],
-        }),
         tailwindcss(),
         devtools({
             removeDevtoolsOnBuild: true,
@@ -23,18 +17,28 @@ export default defineConfig({
             prerender: {
                 enabled: true,
                 crawlLinks: true,
-                filter: ({path}) => {
-                    const isExcludedRoute = [...EXCLUDED_ROUTES].some((route) => path.includes(route))
+                filter: ({ path }) => {
+                    const isExcludedRoute = [...EXCLUDED_ROUTES].some((route) =>
+                        path.includes(route),
+                    );
 
-                    return !isExcludedRoute
-                }
+                    return !isExcludedRoute;
+                },
             },
             sitemap: {
                 enabled: true,
-                host: 'https://aura-historia.com',
+                host: "https://aura-historia.com",
             },
         }),
         viteReact(),
-        cloudflare({viteEnvironment: {name: 'ssr'}}),
+        cloudflare({ viteEnvironment: { name: "ssr" } }),
     ],
-})
+    resolve: {
+        tsconfigPaths: true,
+    },
+    legacy: {
+        // TODO: Report to GA4
+        // https://vite.dev/guide/migration#consistent-commonjs-interop
+        inconsistentCjsInterop: true,
+    },
+});
