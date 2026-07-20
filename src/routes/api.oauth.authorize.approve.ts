76447c1fe9de10ecb265078a4f2321df45fc5ae4
bruderj15 <julian.bruder@outlook.com>
2026-07-20 16:32:43 +0200

    move empty improts to tsconfig prop

--- src/routes/api.oauth.authorize.approve.ts
+++ src/routes/api.oauth.authorize.approve.ts
@@ -1,5 +1,4 @@
 import { createFileRoute } from "@tanstack/react-router";
-import type {} from "@tanstack/start-client-core";
 import { postOAuthAuthorizeApprove } from "@/features/oauth/api/oauthAuthorizeApproveHandler.ts";
 
 export const Route = createFileRoute("/api/oauth/authorize/approve")({
--- src/routes/api.oauth.client_.redirect-broker.woocommerce.ts
+++ src/routes/api.oauth.client_.redirect-broker.woocommerce.ts
@@ -1,6 +1,5 @@
 import { getWooCommerceOAuthClientRedirectBroker } from "@/features/oauth-client-broker/api/oauthClientRedirectBrokerHandler.ts";
 import { createFileRoute } from "@tanstack/react-router";
-import type {} from "@tanstack/start-client-core";
 
 export const Route = createFileRoute("/api/oauth/client_/redirect-broker/woocommerce")({
     server: {
